a60c1269e5c3c5e8e0cc5dc2c551497e639e6370
Colin Chadwick <colinchadwick@hotmail.co.uk>
2022-03-03 14:22:40 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import React from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/orgs/${searchTerm}/repos`)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+        setIsLoading(false);
+      })
+      .catch(console.log);
+  }, [searchTerm]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+
+      {isLoading ? (
+        <>
+          <p>loading...</p>
+        </>
+      ) : (
+        <>
+          {data.map((repo) => {
+            return (
+              <>
+                <p>{repo.name}</p>
+                <p>{repo.url}</p>
+                <p>Stars: {repo.stargazers_count}</p>
+                <p>Forks: {repo.forks_count}</p>
+              </>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
